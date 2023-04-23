588655da3126a1a8eb78239c977dba2845dbfdaf
huda13S <hudasalman0813@gmail.com>
2023-04-23 18:00:43 -0500

    update invokeUrl setting

---
+++ config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_0lgQUSwFE',
+        userPoolClientId: 'YOUR_APP_CLIENT_ID',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://ob811nr50b.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
+
